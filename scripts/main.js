669ba04ca8deea71139ec44ab6f016b5cd3b0212
Antonio R Moses <tonymoses@texasbluesalley.com>
2021-09-15 18:42:49 -0400

    select in navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 import { createApp, reactive } from "https://unpkg.com/petite-vue?module";
 
+
+Vue.component( 'v-select', VueSelect.VueSelect );
+
+
 const store = reactive( {
+  options: {
+    type: ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"],
+  },
   results: null,
   getIconColor (cost, index) {
     const val = (cost * 10) / 2;
     return index <= val ? 'text-green-500' : 'text-gray-300'
   },
-  
-  async getActivity () {
+  async getActivity ( e ) {
+    const formData = new FormData( e.target )
+    const formProps = Object.fromEntries( formData )
+    console.log(formProps)
+    const query = ""
+
     const activity = await fetch(
       `http://www.boredapi.com/api/activity`
     );
