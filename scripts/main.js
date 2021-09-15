06675dc14ff98198f6f85d0cf555cd4f3ffe482f
Antonio R Moses <45946603+StolenThunda@users.noreply.github.com>
2021-09-15 10:09:55 -0400

    Working

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+import { createApp, reactive } from "https://unpkg.com/petite-vue?module";
+
+const store = reactive( {
+  results: null,
+  getIconColor (cost, index) {
+    const val = (cost * 10) / 2;
+    const iconColor = index <= val ? 'text-green-500' : 'text-gray-300'
+
+    console.log( iconColor )
+    return iconColor
+  },
+  getImgByType ( type ) {
+    console.log( type )
+    var svgElement;
+    switch ( type ) {
+      case 'cost':
+        svgElement = '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+        break;
+      case 'education':
+        svgElement = '<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" /></svg>'
+        break;
+      case "social":
+        svgElement = '<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" /></svg>'
+        break
+      case "participants":
+        svgElement = '<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" /></svg>'
+        break;
+      case 'relaxation':
+
+        break;
+      case '':
+        break
+      default:
+        break;
+
+        var parser = new DOMParser();
+        return parser.parseFromString( svgElement, "image/svg+xml" );
+    }
+  },
+  async getActivity () {
+    const activity = await fetch(
+      `http://www.boredapi.com/api/activity/`
+    );
+    const res = await activity.json();
+    console.log( res )
+    this.results = res
+  }
+} );
+createApp( { store } ).mount( '#app' );
