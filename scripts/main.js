38606a03db8e043c5d02a458e5811ff4516296b0
Antonio R Moses <tonymoses@texasbluesalley.com>
2021-09-15 22:24:30 -0400

    FIlters in progress

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,29 @@ import { createApp, reactive } from "https://unpkg.com/petite-vue?module";
 
 
 Vue.component( 'v-select', VueSelect.VueSelect );
-
+const serialize = function ( obj ) {
+  let str = [];
+  for ( let p in obj )
+    
+    if ( obj.hasOwnProperty( p ) ) {
+      if ( obj[p] === '' ) continue
+      str.push( encodeURIComponent( p ) + "=" + encodeURIComponent( obj[p] ) );
+    }
+  const returnStr = str.join( "&" );
+  console.log( 'ret', returnStr )
+  return returnStr !== '' ? '?' + returnStr : returnStr
+}
 
 const store = reactive( {
+  formData: {},
+  getFormData: () => { return this.formData },
   options: {
     type: ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"],
+    price: {
+      min: 0,
+      max: 1,
+      step: 0.1
+    }
   },
   results: null,
   getIconColor (cost, index) {
@@ -14,13 +32,13 @@ const store = reactive( {
     return index <= val ? 'text-green-500' : 'text-gray-300'
   },
   async getActivity ( e ) {
-    const formData = new FormData( e.target )
-    const formProps = Object.fromEntries( formData )
+    this.formData = new FormData( e.target )
+    const formProps = Object.fromEntries( this.formData )
     console.log(formProps)
-    const query = ""
+    
 
     const activity = await fetch(
-      `http://www.boredapi.com/api/activity`
+      `http://www.boredapi.com/api/activity${serialize(formProps)}`
     );
     const res = await activity.json();
     console.log( res )
